77f0235220f6f2bb2fced48deb56977df0357c87
Mavre <mavre@ukr.net>
2013-11-11 16:15:19 +0200

    commit filterResults

--- Framework/src/main/java/com/epam/reutska/helpers/testsupport/TestBase.java
+++ Framework/src/main/java/com/epam/reutska/helpers/testsupport/TestBase.java
@@ -5,13 +5,15 @@ import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
 
 import com.epam.reutska.configuration.TestConfig;
 import com.epam.reutska.helpers.core.WebDriverFactory;
 import com.epam.reutska.pages.MainPage;
-
 
 public class TestBase {
 	protected WebDriver driver;
@@ -20,13 +22,13 @@ public class TestBase {
 
 	protected StringBuffer verificationErrors = new StringBuffer();
 	protected MainPage mainPage;
-	
+
 	@BeforeTest
 	public void init() {
 		TestConfig.setTestName(this.getClass().getSimpleName());
 	}
 
-	@BeforeClass
+	@BeforeMethod
 	public void setUp() throws Exception {
 		driver = WebDriverFactory.getDriver(TestConfig.getCapabilities());
 
@@ -37,9 +39,15 @@ public class TestBase {
 	}
 
 	protected MainPage goToMainPage() {
-		driver.get(baseUrl+"/");
+		driver.get(baseUrl + "/");
 		mainPage = new MainPage(driver);
 		PageFactory.initElements(driver, mainPage);
 		return mainPage;
 	}
+
+	@AfterMethod
+	public void tearDown() throws Exception {
+		driver.quit();
+	}
+
 }
