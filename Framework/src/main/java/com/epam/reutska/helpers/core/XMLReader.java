35bee6373ce79456dc3ab7635a1ad8bfdc8bbb52
Mavre <mavre@ukr.net>
2013-11-13 13:32:07 +0200

    commit file

--- Framework/src/main/java/com/epam/reutska/helpers/core/XMLReader.java
+++ Framework/src/main/java/com/epam/reutska/helpers/core/XMLReader.java
@@ -1,9 +1,13 @@
 package com.epam.reutska.helpers.core;
 
+import java.io.BufferedReader;
+import java.io.CharArrayReader;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.Reader;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.jdom2.Document;
 import org.jdom2.Element;
@@ -25,21 +29,32 @@ public class XMLReader implements IReader{
 	@Override
 	public Object[][] read(int sheetNumber) throws IOException {
 		Object [][]array = null;
+		 Document rDoc = null;
+		  try {
+		   SAXBuilder parser = new SAXBuilder();
 
-		Document rDoc = null;
-		try {
-			SAXBuilder parser = new SAXBuilder();
+		   StringBuilder sb = new StringBuilder();
+		   
+		   try(BufferedReader is = new BufferedReader(new InputStreamReader(new FileInputStream(filePath)))){
+		    String line;
+		    while ((line = is.readLine()) != null){
+		     sb.append(line);
+		    }
+		   }
+		   
+		   String xml = sb.toString();
+		   Matcher junkMatcher = (Pattern.compile("^([\\W]+)<"))
+		                             .matcher( xml.trim() );
+		   xml = junkMatcher.replaceFirst("<");
+		   
+		   rDoc = parser.build(new CharArrayReader(xml.toCharArray()));
+		  } catch (JDOMException | IOException e) {
+		   throw new IOException(e);
+		  }
 
-			Reader in = new InputStreamReader(new FileInputStream(filePath),
-					"UTF-8");
-
-			rDoc = parser.build(in);
-		} catch (JDOMException | IOException e) {
-			throw new IOException(e);
-		}
 	Element page=	rDoc.getRootElement().getChildren("page").get(sheetNumber); 
 	int rowNumber=page.getChildren("line").size();
-	int colNumber=page.getChildren("data").size();
+	int colNumber=page.getChildren("line").get(0).getChildren("data").size();
 	array= new Object[rowNumber][colNumber];
 		for (int i = 0; i < array.length; i++) {
 			for (int j = 0; j < array[i].length; j++) {
@@ -50,6 +65,19 @@ public class XMLReader implements IReader{
 		}
 
 		return array;
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 		
 	}
 
